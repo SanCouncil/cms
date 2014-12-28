3c7e12fda89e6e7a996abec9d2bc0e0497b7b6d8
Matthew Steele <mfsairpwr@gmail.com>
2014-12-28 06:22:02 -0600

    Adding defaults that work to the default.settings.php from VDD

--- drupal7/sites/default/default.settings.php
+++ drupal7/sites/default/default.settings.php
@@ -213,6 +213,14 @@
  * @endcode
  */
 $databases = array();
+$databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => 'drupal7',
+    'username' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'prefix' => '',
+);
 
 /**
  * Access control for update.php script.
