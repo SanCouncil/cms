47cead43ec0f7374c2ca703fa4293bf29e0b498b
Matthew Steele <mfsairpwr@gmail.com>
2014-12-29 14:03:50 -0600

    Updated script

--- upd_drpl7.sh
+++ upd_drpl7.sh
@@ -4,3 +4,5 @@
 rsync -r sananto4@sancouncil.org:~/drupal7.db :~/www/prod/drupal7/sites/default/files/* ~/sites/drupal7/sites/default/files/
 mv ~/sites/drupal7/sites/default/files/drupal7.db ~/
 mysql drupal7 -u root -proot < ~/drupal7.db
+cd ~/sites/drupal7/
+drush updb
