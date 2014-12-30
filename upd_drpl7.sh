47ce53224cba59a638ce0e4b7f67d1764f1a9939
Matthew Steele <mfsairpwr@gmail.com>
2014-12-30 10:51:56 -0600

    Added ! to script

--- upd_drpl7.sh
+++ upd_drpl7.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 # Automatically syncs drupal7 production environment with your dev env
 
 rsync -r sananto4@sancouncil.org:~/drupal7.db :~/www/prod/drupal7/sites/default/files/* ~/sites/drupal7/sites/default/files/
