e007acf429e1566ac024daf3f29db40dca1c786a
Matthew Steele <matthewfsteele@gmail.com>
2014-12-31 13:10:40 +0000

    Fixed update script

--- upd_drpl7.sh
+++ upd_drpl7.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Automatically syncs drupal7 production environment with your dev env
+set -x
 
-ROOT='~/sites/drupal7'
+ROOT=~/sites/drupal7
 SRC='~/www/prod/drupal7'
+
 rsync -r sananto4@sancouncil.org:~/drupal7.db :$SRC/sites/default/files/* $ROOT/sites/default/files/
 mv $ROOT/sites/default/files/drupal7.db ~/
 mysql drupal7 -u root -proot < ~/drupal7.db
-cp $ROOT/sites/default/default.settings.php $ROOT/sites/default/settings.php 
+cp $ROOT/sites/default/default.settings.php  $ROOT/sites/default/settings.php 
 cd $ROOT
 drush updb
