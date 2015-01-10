2396aafe94c3e2aa925ea6fada6bb425705ab58c
Matthew Steele <mfsairpwr@gmail.com>
2015-01-09 21:03:02 -0600

    Changed domain to access host

--- upd_drpl7.sh
+++ upd_drpl7.sh
@@ -4,8 +4,9 @@
 
 ROOT=~/sites/drupal7
 SRC='~/www/prod/drupal7'
+DOMAIN=sanantoniononprofitcouncil.org
 
-rsync -r sananto4@sancouncil.org:~/drupal7.db :$SRC/sites/default/files/* $ROOT/sites/default/files/
+rsync -r sananto4@$DOMAIN:~/drupal7.db :$SRC/sites/default/files/* $ROOT/sites/default/files/
 mv $ROOT/sites/default/files/drupal7.db ~/
 mysql drupal7 -u root -proot < ~/drupal7.db
 cp $ROOT/sites/default/default.settings.php  $ROOT/sites/default/settings.php 
