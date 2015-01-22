e2d1efec2103b4e40be6c211e7aa32f236de5c87
Matthew Steele <mfsairpwr@gmail.com>
2015-01-22 06:19:30 -0600

    Revert "Added code to make registration tpl available and process registration"
    This reverts commit 3a03197d45e37a011e934d2e382a10e9dac8bcbc.
    
    Conflicts:
    	drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php

--- drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php
+++ drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php
@@ -8,34 +8,4 @@
     }
   }
 
-/**
- * Custom Registration Form tpl
- **/
-function SanCouncil_Bootstrap_theme(&$existing, $type, $theme, $path){
-  $hooks = array();
-  // Make user-register.tpl.php available
-  $hooks['user_register_form'] = array (
-    'render element' => 'form',
-    'path' => drupal_get_path('theme','SanCouncil_Bootstrap').'/templates',
-    'template' => 'user-register',
-    'preprocess functions' => 
-    array('SanCouncil_Bootstrap_preprocess_user_register_form'),
-    );
-  return $hooks;
-}
-
-/**
- * Processing user registration
- **/
-function SanCouncil_Bootstrap_preprocess_user_register_form(&$vars) 
-{
-  $args = func_get_args();
-  array_shift($args);
-  $form_state['build_info']['args'] = 
-    $args; 
-  $vars['form'] = 
-    drupal_build_form('user_register_form', 
-    $form_state['build_info']['args']);
-  $vars['intro_text'] = t('This is my super awesome reg form');
-}
 ?>
