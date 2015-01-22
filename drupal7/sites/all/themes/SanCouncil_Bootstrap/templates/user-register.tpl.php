b06432e035b3fb55fe32e364083826228320ed56
Matthew Steele <mfsairpwr@gmail.com>
2015-01-22 06:22:36 -0600

    Revert "started modifications to registration page to add third party sign-in capes"
    This reverts commit 93e300b1f19e91931e7c852c20bf7a549f3178ae.
    
    Conflicts:
    	drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php
    	drupal7/sites/all/themes/SanCouncil_Bootstrap/templates/user-register.tpl.php

--- drupal7/sites/all/themes/SanCouncil_Bootstrap/templates/user-register.tpl.php
+++
@@ -1,6 +0,0 @@
-<p><?php print render($intro_text); ?></p>
-<div class="SanCouncil_Bootstrap-user-login-form-wrapper">
-  <?php print drupal_render_children($form) ?>
-  <?php $region = block_get_blocks_by_region('oauthconn_reg'); ?>
-  <?php print render($region); ?>
-</div>
