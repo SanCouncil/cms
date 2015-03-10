d80b38ad94e4ff9a7a5bd77294ceb147eee9a832
Matthew Steele <mfsairpwr@gmail.com>
2015-03-10 06:11:17 -0500

    Improved styles template and page.tpl -- to be abandoned

--- drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php
+++ drupal7/sites/all/themes/SanCouncil_Bootstrap/template.php
@@ -1,6 +1,6 @@
 <?php
   /**Responsive images for Organizations node*/
-  function SanCouncil_Bootstrap_preprocess_field(&$variables) {
+  /*function SanCouncil_Bootstrap_preprocess_field(&$variables) {
     if($variables['element']['#field_name'] == 'field_icon'){
       foreach($variables['items'] as $key => $item){
         $variables['items'][ $key ]['#item']['attributes']['class'][] = 'img-responsive';
@@ -12,17 +12,12 @@
       }
     }
   }
+   */
 
-/**
- * Bootstrap theme wrapper function for the my menu links.
- */
-function SanCouncil_Bootstrap_menu_tree(&$variables) {
-  return $variables['tree'];
-}
+  function SanCouncil_Bootstrap_preprocess_image(&$variables) {
+    $variables['attributes']['class'][] = 'img-responsive';
+  }
 
-/**
- * Bootstrap theme wrapper function for the my menu links.
- */
-function SanCouncil_Bootstrap_menu_tree__secondary(&$variables) {
-  return $variables['tree'];
+function SanCouncil_Bootstrap_process_page(&$variables) {
+  unset($variables['secondary_nav']['#theme_wrappers']);
 }
